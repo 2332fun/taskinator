5c84a026c04ba800503785e37b72425ae2fbe3fa
DCT <mirachan2332@gmail.com>
2021-12-07 13:20:24 -0600

    task form select added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
-    var listItemEl = document.createElement("li"); //created task item
-    listItemEl.className = "task-item"; //styled task item
-    listItemEl.textContent = "This is a new task."; //added text display
-    tasksToDoEl.appendChild(listItemEl); //appended to task list
-  }
+  var listItemEl = document.createElement("li"); //create new task item
+  listItemEl.className = "task-item"; //style new task item
+  listItemEl.textContent = "This is a new task."; //add text
+  tasksToDoEl.appendChild(listItemEl); //append to task list
+};
 
-  buttonEl.addEventListener("click", createTaskHandler); //on button click, create a task
+buttonEl.addEventListener("click", createTaskHandler);
