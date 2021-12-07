63d61ad2d9d24411679cca7f7512067555f1d7da
DCT <mirachan2332@gmail.com>
2021-12-07 13:29:18 -0600

    made added task no longer disappear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+  event.preventDefault();
   var listItemEl = document.createElement("li"); //create new task item
   listItemEl.className = "task-item"; //style new task item
   listItemEl.textContent = "This is a new task."; //add text
   tasksToDoEl.appendChild(listItemEl); //append to task list
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
